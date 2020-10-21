e9612754580fbb34573e259b5d6294c2cfcc9fd9
carlosmr <carlosmr@users.noreply.github.com>
2020-10-21 13:11:03 -0500

    Update elite-config.js

--- jsa/elite-config.js
+++ jsa/elite-config.js
@@ -1,5 +1,5 @@
 var metacoinConfig = {
-  "contractAddress": "TJDd8couXv87CMxXq9KAT4jUEAVSBLqNgG",
+  "contractAddress": "TP44jH8RoyinS35SV951a9Atd6h6qngP2g",
   "privateKey": "da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0",
-  "fullHost": "https://api.shasta.trongrid.io"
+  "fullHost": "https://api.trongrid.io"
 }
