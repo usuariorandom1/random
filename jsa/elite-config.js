76d61d7142552df22ed938d4665ddc7ba23ecc8f
carlosmr <carlosmr@users.noreply.github.com>
2020-10-21 13:05:09 -0500

    1

--- jsa/elite-config.js
+++
@@ -1,5 +0,0 @@
-var metacoinConfig = {
-  "contractAddress": "TJDd8couXv87CMxXq9KAT4jUEAVSBLqNgG",
-  "privateKey": "da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0",
-  "fullHost": "https://api.shasta.trongrid.io"
-}
