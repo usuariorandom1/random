4ea6fa1fa42f577bef2f8cb02132c2620c7b74be
carlosmr <carlosmr@users.noreply.github.com>
2020-10-21 08:24:20 -0500

    1

--- js2/metacoin-config.js
+++
@@ -1,1 +0,0 @@
-var metacoinConfig={"contractAddress":"TP44jH8RoyinS35SV951a9Atd6h6qngP2g","privateKey":"da146374a75310b9666e834ee4ad0866d6f4035967bfc76217c5a495fff9f0d0","fullHost":"https://api.trongrid.io"}
