7a8a7c4dea388aeb4789b7376352c9fd993c0d24
usuarioRandom <72815503+usuariorandom1@users.noreply.github.com>
2020-10-22 00:34:20 -0500

    Update app.js

--- jsa/app.js
+++ jsa/app.js
@@ -3,8 +3,8 @@ var privateKey
 var tronWeb
 var pay
 var addresact
-const  fullNode = '52.53.189.99';
-const  solidityNode = '52.53.189.99';
+const  fullNode = 'https://api.trongrid.io:8090';
+const  solidityNode = 'https://api.trongrid.io:8091';
 const  eventServer = 'https://api.tronstack.io';
 
 
